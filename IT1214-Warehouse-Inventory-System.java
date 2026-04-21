85d5037dc36a6a0faed63b5c45c40fd0efb5a942
KDSK24 <skaushan671@gmail.com>
2026-04-21 21:08:33 +0530

    Update IT1214-Warehouse-Inventory-System.java

--- IT1214-Warehouse-Inventory-System.java
+++ IT1214-Warehouse-Inventory-System.java
@@ -101,7 +101,7 @@ class Inventory {
     }
 }
 
-public class Warehouse {
+class Warehouse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Inventory inventory = new Inventory();
